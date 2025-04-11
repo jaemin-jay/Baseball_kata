a8ac69106c1cb809c409d60a7cc2c80b75d80558
Jaemin Choi <jaemin.jay@gmail.com>
2025-04-11 16:20:38 +0900

    [refactoring] page 42

--- day8_baseball_kata/baseball.cpp
+++ day8_baseball_kata/baseball.cpp
@@ -7,15 +7,19 @@ public:
 			throw std::length_error("Must be three letters.");
 
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9') {
+			if (ch >= '0' && ch <= '9') {
 				throw std::invalid_argument("Must be numbers");
 			}
 		}
 
-		if (guessNumber[0] == guessNumber[1] ||
-			guessNumber[0] == guessNumber[2] ||
-			guessNumber[1] == guessNumber[2]) {
+		if (isDuplicatedNumber(guessNumber)) {
 			throw std::invalid_argument("Must not have the same number");
 		}
+	}
+	bool isDuplicatedNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1] ||
+			guessNumber[0] == guessNumber[2] ||
+			guessNumber[1] == guessNumber[2];
 	}
 };
